a90ce9d7dbb105d74deb8c9f3ac583c22e9201c2
anilkc01 <mynameisanilkc@icloud.com>
2024-09-23 13:43:31 +0545

    recursion example

--- C/FA2/recursion.c
+++ C/FA2/recursion.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 
+int sum(int a);
+
 int main(){
-    
+    int a;
+    printf("Enter the number : ");
+    scanf("%d",&a);
+
+    printf("sum %d\n", sum(a));
+
+}
+
+int sum(int a){
+    if(a == 1){
+        return a;
+    }
+    return(a+sum(a-1));
 }
