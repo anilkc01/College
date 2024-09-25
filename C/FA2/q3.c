c1c7d574b558ea66a5f45307eb1b99bff9093b6d
anilkc01 <mynameisanilkc@icloud.com>
2024-09-25 13:36:07 +0545

    fibonacci series

--- C/FA2/q3.c
+++ C/FA2/q3.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+void getfib(int, int, int);
 int main(){
+    int count = 10;
+
+    int a = 0;
+    int b = 1;
+
+    printf("%d ", a);
+    printf("%d ", b);
+
+    getfib(a,b,count-2);
+}
+
+void getfib(int a, int b, int count){
+    if(count >= 0){
+        printf("%d ",a+b);
+        getfib(b,a+b,--count);
+    }
+
+   
     
 }
