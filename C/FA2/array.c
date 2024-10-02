840e81eb2259e9331454430bec2d704b2ef0da05
anilkc01 <mynameisanilkc@icloud.com>
2024-10-02 12:58:09 +0545

    array

---
+++ C/FA2/array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a[5] = {1,2,3,4,5};
+    int highest = 0;
+    for(int i = 0; i<5; i++){
+        printf("Enter the value for index %d : ",i);
+        scanf("%d",&a[i]);
+        highest = a[i] > highest ? a[i] : highest;
+    }
+
+    printf("The highest you entered is %d\n", highest);
+}
